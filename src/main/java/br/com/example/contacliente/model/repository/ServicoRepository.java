14355223fb86616c98ec27d6410057c5ceddd2fe
guilherme-aor <g.augustooliv200@gmail.com>
2021-11-03 13:18:25 -0300

    commit

--- src/main/java/br/com/example/contacliente/model/repository/ServicoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.example.contacliente.model.repository;
-
-import br.com.example.contacliente.model.entity.Servico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicoRepository extends JpaRepository<Servico, Integer> {
-}
---
+++ src/main/java/br/com/example/contacliente/rest/ServicoPrestadoController.java
@@ -0,0 +1,59 @@
+package br.com.example.contacliente.rest;
+
+import br.com.example.contacliente.model.entity.Cliente;
+import br.com.example.contacliente.model.entity.ServicoPrestado;
+import br.com.example.contacliente.model.repository.ClienteRepository;
+import br.com.example.contacliente.model.repository.ServicoPrestadoRepository;
+import br.com.example.contacliente.rest.dto.ServicoPrestadoDTO;
+import br.com.example.contacliente.util.BigDecimalConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/servicos-prestados")
+@RequiredArgsConstructor
+public class ServicoPrestadoController {
+
+    private final ClienteRepository clienteRepository;
+    private final ServicoPrestadoRepository repository;
+    private final BigDecimalConverter bigDecimalConverter;
+
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO dto){
+        LocalDate data = LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyy"));
+        Integer idCliente = dto.getIdCliente();
+
+        Cliente cliente =
+                clienteRepository
+                        .findById(idCliente)
+                        .orElseThrow(() -> new ResponseStatusException(
+                                HttpStatus.BAD_REQUEST,"Cliente inexistente"));
+
+
+        ServicoPrestado servicoPrestado= new ServicoPrestado();
+        servicoPrestado.setDescricao(dto.getDescricao());
+        servicoPrestado.setData( data );
+        servicoPrestado.setCliente(cliente);
+        servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
+
+        return repository.save(servicoPrestado);
+    }
+
+    @GetMapping
+    public List<ServicoPrestado> pesquisar(
+            @RequestParam( value = "nome", required = false, defaultValue = "") String nome,
+            @RequestParam( value = "mes", required = false ) Integer mes
+    ){
+        return repository.findByNomeClienteAndMes("%" + nome + "%", mes);
+    }
+}

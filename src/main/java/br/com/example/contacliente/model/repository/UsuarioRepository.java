07c59e32e16fc5763c64b136536a9de1fcd4840c
guilherme-aor <g.augustooliv200@gmail.com>
2021-12-21 17:23:58 -0300

    commit

--- src/main/java/br/com/example/contacliente/model/repository/UsuarioRepository.java
+++ src/main/java/br/com/example/contacliente/model/repository/UsuarioRepository.java
@@ -3,5 +3,9 @@ package br.com.example.contacliente.model.repository;
 import br.com.example.contacliente.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByUsername(String username);
 }

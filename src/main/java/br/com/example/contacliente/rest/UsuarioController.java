26577a639d70c24b56a243257541c596333e19c3
guilherme-aor <g.augustooliv200@gmail.com>
2021-12-23 13:06:07 -0300

    commit

--- src/main/java/br/com/example/contacliente/rest/UsuarioController.java
+++ src/main/java/br/com/example/contacliente/rest/UsuarioController.java
@@ -1,10 +1,13 @@
 package br.com.example.contacliente.rest;
 
+import br.com.example.contacliente.exception.UsuarioCadastradoException;
 import br.com.example.contacliente.model.entity.Usuario;
 import br.com.example.contacliente.model.repository.UsuarioRepository;
+import br.com.example.contacliente.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -13,12 +16,17 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class UsuarioController {
 
-    private  final UsuarioRepository repository;
+    private final UsuarioService service;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void salvar(@RequestBody @Valid Usuario usuario){
-        repository.save(usuario);
+        try {
+            service.salvar(usuario);
+        }catch (UsuarioCadastradoException e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+
     }
 
 }
